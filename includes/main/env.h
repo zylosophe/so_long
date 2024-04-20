a395676ee9369460e84cb65493dda01aa32993ab
mcolonna <mcolonna@student.42perpignan.fr>
2024-04-20 12:51:24 +0200

    norminette

--- includes/main/env.h
+++ includes/main/env.h
@@ -6,7 +6,7 @@
 /*   By: mcolonna <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/26 15:26:13 by mcolonna          #+#    #+#             */
-/*   Updated: 2024/04/19 23:08:32 by mcolonna         ###   ########.fr       */
+/*   Updated: 2024/04/20 12:47:22 by mcolonna         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,7 @@
 # define TEXT_SPACE 9
 
 # define LEVEL_COUNT 10
+
 extern const t_const_string	g_levels[LEVEL_COUNT];
 
 typedef struct s_env
@@ -50,6 +51,6 @@ typedef struct s_env
 	int						moves;
 }					t_env;
 
-extern t_env	g_env;
+extern t_env				g_env;
 
 #endif
--- src/main/env.c
+++ src/main/env.c
@@ -6,23 +6,23 @@
 /*   By: mcolonna <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/26 16:32:36 by mcolonna          #+#    #+#             */
-/*   Updated: 2024/04/20 12:17:48 by mcolonna         ###   ########.fr       */
+/*   Updated: 2024/04/20 12:48:15 by mcolonna         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "includes.h"
 
-const t_const_string g_levels[LEVEL_COUNT] = {
-"rooms/01.ber",
-"rooms/02.ber",
-"rooms/03.ber",
-"rooms/04.ber",
-"rooms/05.ber",
-"rooms/06.ber",
-"rooms/07.ber",
-"rooms/08.ber",
-"rooms/09.ber",
-"rooms/10.ber",
+const t_const_string	g_levels[LEVEL_COUNT] = {
+	"rooms/01.ber",
+	"rooms/02.ber",
+	"rooms/03.ber",
+	"rooms/04.ber",
+	"rooms/05.ber",
+	"rooms/06.ber",
+	"rooms/07.ber",
+	"rooms/08.ber",
+	"rooms/09.ber",
+	"rooms/10.ber",
 };
 
-t_env	g_env;
+t_env					g_env;
