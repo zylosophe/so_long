56c7f423908e723b3d52081209804a293678f333
mcolonna <mcolonna@student.42perpignan.fr>
2024-04-23 01:21:48 +0200

    save: for windows

--- includes/includes.h
+++ includes/includes.h
@@ -6,13 +6,39 @@
 /*   By: mcolonna <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/02 13:53:41 by mcolonna          #+#    #+#             */
-/*   Updated: 2024/04/20 13:32:27 by mcolonna         ###   ########.fr       */
+/*   Updated: 2024/04/23 01:11:25 by mcolonna         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef INCLUDES_H
 # define INCLUDES_H
 
+# define XK_Left 0xFF51
+# define XK_Up 0xFF52
+# define XK_Right 0xFF53
+# define XK_Down 0xFF54
+# define XK_S 0x0053
+# define XK_Z 0x005a
+# define XK_Q 0x0051
+# define XK_D 0x0044
+# define XK_W 0x0057
+# define XK_A 0x0041
+# define XK_s XK_S + 0x20
+# define XK_z XK_Z + 0x20
+# define XK_q XK_Q + 0x20
+# define XK_d XK_D + 0x20
+# define XK_w XK_W + 0x20
+# define XK_a XK_A + 0x20
+# define XK_Escape 0xFF1B
+# define XK_Return 0xFF0D
+# define DestroyNotify 17
+# define StructureNotifyMask 1L<<17
+# define KeyPress 2
+# define KeyRelease 3
+# define KeyPressMask 1L<<0
+# define KeyReleaseMask 1L<<1
+
+# include <windows.h>
 # include <math.h>
 # include <fcntl.h>
 # include <time.h>
@@ -21,8 +47,6 @@
 # include <stdlib.h>
 # include <stddef.h>
 # include <stdint.h>
-# include <X11/keysym.h>
-# include <X11/Xlib.h>
 # include <errno.h>
 # include "libtf.h"
 # include "mlx.h"
